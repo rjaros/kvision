4495ca4af667b5234f154196059cf636d415ad86
Robert Jaros <rjaros@finn.pl>
2018-02-20 11:59:26 +0100

    Change abstract class to interface for ApplicationBase.

--- src/main/kotlin/pl/treksoft/kvision/hmr/ApplicationBase.kt
+++ src/main/kotlin/pl/treksoft/kvision/hmr/ApplicationBase.kt
@@ -22,20 +22,20 @@
 package pl.treksoft.kvision.hmr
 
 /**
- * Base class for applications.
+ * Base interface for applications.
  *
- * Base class for applications supporting Hot Module Replacement (HMR).
+ * Base interface for applications supporting Hot Module Replacement (HMR).
  */
-abstract class ApplicationBase {
+interface ApplicationBase {
     /**
      * Starting point for an application.
      * @param state Initial state between Hot Module Replacement (HMR).
      */
-    abstract fun start(state: Map<String, Any>)
+    fun start(state: Map<String, Any>)
 
     /**
      * Ending point for an application.
      * @return final state for Hot Module Replacement (HMR).
      */
-    abstract fun dispose(): Map<String, Any>
+    fun dispose(): Map<String, Any>
 }
