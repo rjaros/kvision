498af9e696cc6aa9c511cb6a8ff9be9c43e31280
Robert Jaros <rjaros@finn.pl>
2020-06-12 16:14:07 +0200

    OnsenUI dialog components (#126)

---
+++ kvision-modules/kvision-onsenui/src/test/kotlin/test/pl/treksoft/kvision/onsenui/dialog/ToastSpec.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2017-present Robert Jaros
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package test.pl.treksoft.kvision.onsenui.dialog
+
+import pl.treksoft.kvision.html.div
+import pl.treksoft.kvision.onsenui.dialog.toast
+import pl.treksoft.kvision.panel.ContainerType
+import pl.treksoft.kvision.panel.Root
+import test.pl.treksoft.kvision.DomSpec
+import kotlin.browser.document
+import kotlin.test.Test
+
+class ToastSpec : DomSpec {
+
+    @Test
+    fun render() {
+        run {
+            val root = Root("test", containerType = ContainerType.FIXED)
+            val toast = root.toast {
+                div("test")
+            }
+            toast.showToast()
+            val element = document.getElementById("test")
+            assertEqualsHtml(
+                "<ons-toast style=\"display: block; z-index: 10000;\"><div class=\"toast\" style=\"transform: translate3d(0px, 100%, 0px); opacity: 0;\"><div class=\"toast__message\"><div>test</div></div></div></ons-toast>",
+                element?.innerHTML?.replace(Regex("data-device-back-button-handler-id=\"[0-9]+\""), ""),
+                "Should render Onsen UI toast component"
+            )
+            toast.hideToast()
+        }
+    }
+}
