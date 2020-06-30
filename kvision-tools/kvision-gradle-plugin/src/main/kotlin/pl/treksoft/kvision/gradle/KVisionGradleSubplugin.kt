11baeffb8947e1ab57e4aa9c02ade0b52eaa3edd
Robert Jaros <rjaros@finn.pl>
2020-06-30 19:27:40 +0200

    Version 3.11.0

--- kvision-tools/kvision-gradle-plugin/src/main/kotlin/pl/treksoft/kvision/gradle/KVisionGradleSubplugin.kt
+++ kvision-tools/kvision-gradle-plugin/src/main/kotlin/pl/treksoft/kvision/gradle/KVisionGradleSubplugin.kt
@@ -49,7 +49,7 @@ class KVisionGradleSubplugin : KotlinGradleSubplugin<AbstractCompile> {
     override fun getPluginArtifact(): SubpluginArtifact = SubpluginArtifact(
         groupId = "pl.treksoft",
         artifactId = "kvision-compiler-plugin",
-        version = "3.10.2"
+        version = "3.11.0"
     )
 
 }
