52ee09682609ac68c1844d7eb3358f7f3b387222
Martynas Petuška <petuska.m@cambridgeassessment.org.uk>
2020-10-14 17:38:37 +0100

    package.json management in gradle for assets

--- kvision-assets/build.gradle.kts
+++ kvision-assets/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("lt.petuska.npm.publish") version "0.1.0"
+  id("lt.petuska.npm.publish") version "0.1.1"
 }
 
 npmPublishing {
@@ -9,7 +9,24 @@ npmPublishing {
       readme = file("README.md")
       files {
         from(projectDir) {
-          include("css/**", "img/**", "js/**", "index.js", "package.json")
+          include("css/**", "img/**", "js/**", "index.js")
+        }
+      }
+      packageJson {
+        version = "1.0.1"
+        description = "The assets for the KVision framework"
+        keywords = jsonArray("kvision", "kotlin")
+        homepage = "https://github.com/rjaros/kvision#readme"
+        license = "MIT"
+        repository {
+          type = "git"
+          url = "git+https://github.com/rjaros/kvision.git"
+        }
+        author {
+          name = "Robert Jaros"
+        }
+        bugs {
+          url = "https://github.com/rjaros/kvision/issues"
         }
       }
     }
