bffbefd43aa8920ed33f3b02774f58aa390078e9
Robert Jaros <rjaros@finn.pl>
2020-12-19 01:35:30 +0100

    Fix String serialization to match original behaviour

--- kvision-modules/kvision-common-remote/src/jvmTest/kotlin/pl/treksoft/kvision/remote/JacksonObjectDeSerializerKtTest.kt
+++ kvision-modules/kvision-common-remote/src/jvmTest/kotlin/pl/treksoft/kvision/remote/JacksonObjectDeSerializerKtTest.kt
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2017-present Robert Jaros
+ * Copyright (c) 2020 Yannik Hampe
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package pl.treksoft.kvision.remote
 
 import org.hamcrest.MatcherAssert.assertThat
@@ -28,8 +50,8 @@ class JacksonObjectDeSerializerKtTest {
     
     @DataProvider
     fun provide_obj_expectedString(): Array<Array<Any?>> = arrayOf(
-        arrayOf("simple string", "simple string"),
-        arrayOf("special {[]} chars", "special {[]} chars"),
+        arrayOf("simple string", "\"simple string\""),
+        arrayOf("special {[]} chars", "\"special {[]} chars\""),
         arrayOf(42, "42"),
         arrayOf("firstValue" to "secondValue", """{"first":"firstValue","second":"secondValue"}"""),
         arrayOf(null, null)
