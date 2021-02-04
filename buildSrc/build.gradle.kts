ac3d92099052b5f281d5819b52bf2ff347c87d17
Robert Jaros <rjaros@finn.pl>
2021-02-04 14:52:21 +0100

    Dependencies upgrade: Kotlin 1.4.30, Serialization 1.1.0-RC, Ktor 1.5.1, Javalin 3.13.3, Vert.x 4.0.1, Micronaut 2.3.1

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,6 +9,6 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
     implementation(gradleApi())
 }
