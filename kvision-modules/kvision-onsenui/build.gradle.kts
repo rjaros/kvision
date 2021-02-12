1cebf4f2c935581dab94b95e43826aabe75ce4a0
Robert Jaros <rjaros@finn.pl>
2021-02-12 08:48:31 +0100

    Upgrade OnsenUI to 2.11.2

--- kvision-modules/kvision-onsenui/build.gradle.kts
+++ kvision-modules/kvision-onsenui/build.gradle.kts
@@ -11,7 +11,7 @@ kotlin {
 
 dependencies {
     api(rootProject)
-    implementation(npm("onsenui", "^2.11.1"))
+    implementation(npm("onsenui", "^2.11.2"))
     testImplementation(kotlin("test-js"))
     testImplementation(project(":kvision-modules:kvision-testutils"))
 }
