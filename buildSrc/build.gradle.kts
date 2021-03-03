a5a801fde010cbe14054bec4989504a45f17a788
Robert Jaros <rjaros@finn.pl>
2021-03-03 17:20:36 +0100

    Upgrade Kotlin to 1.4.31, coroutines to 1.4.3, Ktor to 1.5.2

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.31")
     implementation("de.marcphilipp.gradle:nexus-publish-plugin:0.4.0")
     implementation(gradleApi())
 }
