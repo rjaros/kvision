4b9259dd1ed3e9fb803ec38c4eead8aef9df663c
Robert Jaros <rjaros@finn.pl>
2021-04-04 00:50:56 +0200

    Upgrade Kotlin to 1.4.32 and Micronaut to 2.4.2

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.31")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
     implementation("de.marcphilipp.gradle:nexus-publish-plugin:0.4.0")
     implementation(gradleApi())
 }
