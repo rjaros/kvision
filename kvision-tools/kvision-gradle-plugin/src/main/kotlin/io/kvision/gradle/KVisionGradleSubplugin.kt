760b5f6741fff4bb53871c931aaef8e75992e2b8
Robert Jaros <rjaros@finn.pl>
2021-05-31 18:40:22 +0200

    Version 4.7.2

--- kvision-tools/kvision-gradle-plugin/src/main/kotlin/io/kvision/gradle/KVisionGradleSubplugin.kt
+++ kvision-tools/kvision-gradle-plugin/src/main/kotlin/io/kvision/gradle/KVisionGradleSubplugin.kt
@@ -42,7 +42,7 @@ class KVisionGradleSubplugin : KotlinCompilerPluginSupportPlugin {
     override fun getPluginArtifact(): SubpluginArtifact = SubpluginArtifact(
         groupId = "io.kvision",
         artifactId = "kvision-compiler-plugin",
-        version = "4.7.1"
+        version = "4.7.2"
     )
 
     override fun isApplicable(kotlinCompilation: KotlinCompilation<*>): Boolean {
