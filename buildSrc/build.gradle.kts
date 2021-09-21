a3c41238571f3a49a4b504e704f72e40b8195b91
Robert Jaros <rjaros@finn.pl>
2021-09-21 06:28:50 +0200

    Upgrade Kotlin to 1.5.31.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
     implementation("de.marcphilipp.gradle:nexus-publish-plugin:0.4.0")
     implementation(gradleApi())
 }
