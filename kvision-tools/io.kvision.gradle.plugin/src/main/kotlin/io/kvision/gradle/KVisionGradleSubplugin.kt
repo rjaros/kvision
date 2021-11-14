fe97d1f53b72dd3b4be18838d895afd75a1a1fa2
Robert Jaros <rjaros@finn.pl>
2021-11-14 19:11:25 +0100

    Upgrade webpack and webpack-dev-server

--- kvision-tools/io.kvision.gradle.plugin/src/main/kotlin/io/kvision/gradle/KVisionGradleSubplugin.kt
+++ kvision-tools/io.kvision.gradle.plugin/src/main/kotlin/io/kvision/gradle/KVisionGradleSubplugin.kt
@@ -84,8 +84,8 @@ class KVisionGradleSubplugin : KotlinCompilerPluginSupportPlugin {
                     }
                 }
                 rootProject.extensions.configure<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension> {
-                    versions.webpackDevServer.version = "4.4.0"
-                    versions.webpack.version = "5.61.0"
+                    versions.webpackDevServer.version = "4.5.0"
+                    versions.webpack.version = "5.64.0"
                     versions.webpackCli.version = "4.9.1"
                 }
             }
@@ -114,8 +114,8 @@ class KVisionGradleSubplugin : KotlinCompilerPluginSupportPlugin {
                     }
                 }
                 rootProject.extensions.configure<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension> {
-                    versions.webpackDevServer.version = "4.4.0"
-                    versions.webpack.version = "5.61.0"
+                    versions.webpackDevServer.version = "4.5.0"
+                    versions.webpack.version = "5.64.0"
                     versions.webpackCli.version = "4.9.1"
                 }
             }
