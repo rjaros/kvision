2ec81b7196bdd1e2941239ddac12ff69c6da8a73
Adam <897017+aSemy@users.noreply.github.com>
2022-01-01 14:21:35 +0100

    tidy up LeafletMap, remove duplicated functions, and remove KDoc (it wasn't really necessary)

--- kvision-modules/kvision-maps/src/main/kotlin/externals/leaflet/map/LeafletMap.kt
+++ kvision-modules/kvision-maps/src/main/kotlin/externals/leaflet/map/LeafletMap.kt
@@ -46,117 +46,59 @@ open external class LeafletMap : Evented {
     open var options: MapOptions
 
     open fun getRenderer(layer: Path): Renderer
-    /** @return this */
+
     open fun addControl(control: Control): LeafletMap
-    /** @return this */
     open fun removeControl(control: Control): LeafletMap
-    /** @return this */
+
     open fun addLayer(layer: Layer): LeafletMap
-    /** @return this */
     open fun removeLayer(layer: Layer): LeafletMap
     open fun hasLayer(layer: Layer): Boolean
-    /** @return this */
     open fun eachLayer(fn: (layer: Layer) -> Unit, context: Any = definedExternally): LeafletMap
-    /** @return this */
+
     open fun openPopup(popup: Popup): LeafletMap
-    /** @return this */
     open fun openPopup(content: String, latlng: LatLng, options: PopupOptions = definedExternally): LeafletMap
-    /** @return this */
-    open fun openPopup(content: String, latlng: LatLng): LeafletMap
-    /** @return this */
     open fun openPopup(content: HTMLElement, latlng: LatLng, options: PopupOptions = definedExternally): LeafletMap
-    /** @return this */
-    open fun openPopup(content: HTMLElement, latlng: LatLng): LeafletMap
-    /** @return this */
     open fun closePopup(popup: Popup = definedExternally): LeafletMap
-    /** @return this */
+
     open fun openTooltip(tooltip: Tooltip): LeafletMap
-    /** @return this */
     open fun openTooltip(content: String, latlng: LatLng, options: TooltipOptions = definedExternally): LeafletMap
-    /** @return this */
-    open fun openTooltip(content: String, latlng: LatLng): LeafletMap
-    /** @return this */
     open fun openTooltip(content: HTMLElement, latlng: LatLng, options: TooltipOptions = definedExternally): LeafletMap
-    /** @return this */
-    open fun openTooltip(content: HTMLElement, latlng: LatLng): LeafletMap
-    /** @return this */
     open fun closeTooltip(tooltip: Tooltip = definedExternally): LeafletMap
-    /** @return this */
+
     open fun setView(center: LatLng, zoom: Number = definedExternally, options: ZoomPanOptions = definedExternally): LeafletMap
-    /** @return this */
-    open fun setView(center: LatLng): LeafletMap
-    /** @return this */
-    open fun setView(center: LatLng, zoom: Number = definedExternally): LeafletMap
-    /** @return this */
+
     open fun setZoom(zoom: Number, options: ZoomPanOptions = definedExternally): LeafletMap
-    /** @return this */
     open fun zoomIn(delta: Number = definedExternally, options: ZoomOptions = definedExternally): LeafletMap
-    /** @return this */
     open fun zoomOut(delta: Number = definedExternally, options: ZoomOptions = definedExternally): LeafletMap
-    /** @return this */
     open fun setZoomAround(position: Point, zoom: Number, options: ZoomOptions = definedExternally): LeafletMap
-    /** @return this */
-    open fun setZoomAround(position: Point, zoom: Number): LeafletMap
-    /** @return this */
     open fun setZoomAround(position: LatLng, zoom: Number, options: ZoomOptions = definedExternally): LeafletMap
-    /** @return this */
-    open fun setZoomAround(position: LatLng, zoom: Number): LeafletMap
-    /** @return this */
+
     open fun fitBounds(bounds: LatLngBounds, options: FitBoundsOptions = definedExternally): LeafletMap
-    /** @return this */
-    open fun fitBounds(bounds: LatLngBounds): LeafletMap
-    /** @return this */
     open fun fitWorld(options: FitBoundsOptions = definedExternally): LeafletMap
-    /** @return this */
+
     open fun panTo(latlng: LatLng, options: PanOptions = definedExternally): LeafletMap
-    /** @return this */
-    open fun panTo(latlng: LatLng): LeafletMap
-    /** @return this */
     open fun panBy(offset: Point, options: PanOptions = definedExternally): LeafletMap
-    /** @return this */
-    open fun panBy(offset: Point): LeafletMap
-    /** @return this */
+
     open fun setMaxBounds(bounds: LatLngBounds): LeafletMap
-    /** @return this */
     open fun setMinZoom(zoom: Number): LeafletMap
-    /** @return this */
     open fun setMaxZoom(zoom: Number): LeafletMap
-    /** @return this */
+
     open fun panInside(latLng: LatLng, options: PanInsideOptions = definedExternally): LeafletMap
-    /** @return this */
-    open fun panInside(latLng: LatLng): LeafletMap
-    /** @return this */
     open fun panInsideBounds(bounds: LatLngBounds, options: PanOptions = definedExternally): LeafletMap
-    /** @return this */
-    open fun panInsideBounds(bounds: LatLngBounds): LeafletMap
-    /** @return this */
+
     open fun invalidateSize(options: Boolean = definedExternally): LeafletMap
-    /** @return this */
-    open fun invalidateSize(): LeafletMap
-    /** @return this */
     open fun invalidateSize(options: InvalidateSizeOptions = definedExternally): LeafletMap
-    /** @return this */
     open fun stop(): LeafletMap
-    /** @return this */
+
     open fun flyTo(latlng: LatLng, zoom: Number = definedExternally, options: ZoomPanOptions = definedExternally): LeafletMap
-    /** @return this */
-    open fun flyTo(latlng: LatLng): LeafletMap
-    /** @return this */
-    open fun flyTo(latlng: LatLng, zoom: Number = definedExternally): LeafletMap
-    /** @return this */
     open fun flyToBounds(bounds: LatLngBounds, options: FitBoundsOptions = definedExternally): LeafletMap
-    /** @return this */
-    open fun flyToBounds(bounds: LatLngBounds): LeafletMap
-    /** @return this */
     open fun addHandler(name: String, HandlerClass: Any): LeafletMap
-    /** @return this */
     open fun remove(): LeafletMap
     open fun createPane(name: String, container: HTMLElement = definedExternally): HTMLElement
     open fun getPane(pane: String): HTMLElement?
     open fun getPane(pane: HTMLElement): HTMLElement?
 //    open fun getPanes(): `T$5` /* `T$5` & DefaultMapPanes */
     open fun getContainer(): HTMLElement
-    /** @return this */
     open fun whenReady(fn: () -> Unit, context: Any = definedExternally): LeafletMap
     open fun getCenter(): LatLng
     open fun getZoom(): Number
@@ -164,8 +106,6 @@ open external class LeafletMap : Evented {
     open fun getMinZoom(): Number
     open fun getMaxZoom(): Number
     open fun getBoundsZoom(bounds: LatLngBounds, inside: Boolean = definedExternally, padding: Point = definedExternally): Number
-    open fun getBoundsZoom(bounds: LatLngBounds): Number
-    open fun getBoundsZoom(bounds: LatLngBounds, inside: Boolean = definedExternally): Number
     open fun getSize(): Point
     open fun getPixelBounds(): Bounds
     open fun getPixelOrigin(): Point
@@ -173,9 +113,7 @@ open external class LeafletMap : Evented {
     open fun getZoomScale(toZoom: Number, fromZoom: Number = definedExternally): Number
     open fun getScaleZoom(scale: Number, fromZoom: Number = definedExternally): Number
     open fun project(latlng: LatLng, zoom: Number = definedExternally): Point
-    open fun project(latlng: LatLng): Point
     open fun unproject(point: Point, zoom: Number = definedExternally): LatLng
-    open fun unproject(point: Point): LatLng
     open fun layerPointToLatLng(point: Point): LatLng
     open fun latLngToLayerPoint(latlng: LatLng): Point
     open fun wrapLatLng(latlng: LatLng): LatLng
@@ -188,16 +126,6 @@ open external class LeafletMap : Evented {
     open fun mouseEventToContainerPoint(ev: MouseEvent): Point
     open fun mouseEventToLayerPoint(ev: MouseEvent): Point
     open fun mouseEventToLatLng(ev: MouseEvent): LatLng
-    /** @return this */
     open fun locate(options: LocateOptions = definedExternally): LeafletMap
-    /** @return this */
     open fun stopLocate(): LeafletMap
 }
-
-//external fun map(element: String, options: MapOptions = definedExternally): Map
-//
-//external fun map(element: String): Map
-//
-//external fun map(element: HTMLElement, options: MapOptions = definedExternally): Map
-//
-//external fun map(element: HTMLElement): Map
