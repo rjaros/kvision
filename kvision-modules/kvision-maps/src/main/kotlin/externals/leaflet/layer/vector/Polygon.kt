9be73eeed5337ed2f557580192e879eb6a8b8ded
Adam <897017+aSemy@users.noreply.github.com>
2022-01-01 14:18:57 +0100

    use LatLng for constructors, because it's more clear than Any, and the other two types aren't used by this implementation

--- kvision-modules/kvision-maps/src/main/kotlin/externals/leaflet/layer/vector/Polygon.kt
+++ kvision-modules/kvision-maps/src/main/kotlin/externals/leaflet/layer/vector/Polygon.kt
@@ -3,21 +3,23 @@
 
 package externals.leaflet.layer.vector
 
+import externals.leaflet.geo.LatLng
 
-open external class Polygon<P : Any> :
-    Polyline<dynamic /* geojson.Polygon | geojson.MultiPolygon */, P> {
+
+open external class Polygon<P : Any>
+    : Polyline<dynamic /* geojson.Polygon | geojson.MultiPolygon */, P> {
     constructor(
-        latlngs: Array<Any /* LatLng | LatLngLiteral | JsTuple<Number, Number> */>,
+        latlngs: Array<LatLng /* LatLng | LatLngLiteral | JsTuple<Number, Number> */>,
         options: PolylineOptions = definedExternally
     )
     //    constructor(latlngs: Array<Any /* LatLng | LatLngLiteral | JsTuple<Number, Number> */>)
     constructor(
-        latlngs: Array<Array<Any /* LatLng | LatLngLiteral | JsTuple<Number, Number> */>>,
+        latlngs: Array<Array<LatLng /* LatLng | LatLngLiteral | JsTuple<Number, Number> */>>,
         options: PolylineOptions = definedExternally
     )
     //    constructor(latlngs: Array<Array<Any /* LatLng | LatLngLiteral | JsTuple<Number, Number> */>>)
     constructor(
-        latlngs: Array<Array<Array<Any /* LatLng | LatLngLiteral | JsTuple<Number, Number> */>>>,
+        latlngs: Array<Array<Array<LatLng /* LatLng | LatLngLiteral | JsTuple<Number, Number> */>>>,
         options: PolylineOptions = definedExternally
     )
 //    constructor(latlngs: Array<Array<Array<Any /* LatLng | LatLngLiteral | JsTuple<Number, Number> */>>>)
