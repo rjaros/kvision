0c881f7983086cb78d1f8391c418fc38d573d357
Robert Jaros <rjaros@finn.pl>
2022-04-19 09:49:22 +0200

    Upgrade Dokka to 1.6.20

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,6 +10,6 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20")
     implementation("de.marcphilipp.gradle:nexus-publish-plugin:0.4.0")
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.6.10")
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.6.20")
     implementation(gradleApi())
 }
