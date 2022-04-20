17a1db2783e4caeb69244547223a3668050e78d1
Adam <897017+aSemy@users.noreply.github.com>
2022-04-20 19:21:36 +0200

    update Projection docs

--- kvision-modules/kvision-maps/src/main/kotlin/io/kvision/maps/externals/leaflet/geo/Projection.kt
+++ kvision-modules/kvision-maps/src/main/kotlin/io/kvision/maps/externals/leaflet/geo/Projection.kt
@@ -28,16 +28,54 @@ package io.kvision.maps.externals.leaflet.geo
 import io.kvision.maps.externals.leaflet.geometry.Bounds
 import io.kvision.maps.externals.leaflet.geometry.Point
 
-
+/**
+ * An object with methods for projecting geographical coordinates of the world onto a flat surface
+ * (and back).
+ *
+ * https://leafletjs.com/SlavaUkraini/reference.html#projection
+ */
 external class Projection {
+
+    /** The bounds (specified in CRS units) where the projection is valid */
     var bounds: Bounds
 
+    /**
+     * Projects geographical coordinates into a 2D point. Only accepts actual [LatLng] instances,
+     * not arrays.
+     */
     fun project(latlng: LatLng): Point
+
+    /**
+     * The inverse of [project]. Projects a 2D point into a geographical location. Only accepts
+     * actual [Point] instances, not arrays.
+     *
+     * Note that the projection instances do not inherit from Leaflet's
+     * [Class][io.kvision.maps.externals.leaflet.core.Class] object, and can't be instantiated.
+     * Also, new classes can't inherit from them, and methods can't be added to them with the include
+     * function.
+     */
     fun unproject(point: Point): LatLng
 
     companion object {
+        /**
+         * Equirectangular, or Plate Carree projection — the most simple projection, mostly used by
+         * GIS enthusiasts. Directly maps `x` as longitude, and `y` as latitude. Also suitable for
+         * flat worlds, e.g. game maps. Used by the
+         * [EPSG3857][io.kvision.maps.externals.leaflet.geo.CRS.EPSG3857] and
+         * [Simple][io.kvision.maps.externals.leaflet.geo.CRS.Simple] CRS.
+         */
         val LonLat: Projection
+        /**
+         * Elliptical Mercator projection — more complex than Spherical Mercator. Assumes that
+         * Earth is an ellipsoid. Used by the
+         * [EPSG3857][io.kvision.maps.externals.leaflet.geo.CRS.EPSG3857] CRS.
+         */
         val Mercator: Projection
+        /**
+         * Spherical Mercator projection — the most common projection for online maps, used by
+         * almost all free and commercial tile providers. Assumes that Earth is a sphere.
+         * Used by the [EPSG3857][io.kvision.maps.externals.leaflet.geo.CRS.EPSG3857] CRS.
+         */
         val SphericalMercator: Projection
     }
 }
