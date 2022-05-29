5b72ba9cc9833782933bea1002276f5749662890
Robert Jaros <rjaros@finn.pl>
2022-05-29 19:18:20 +0200

    Upgrade Tabulator to 5.2.5

--- kvision-modules/kvision-tabulator/build.gradle.kts
+++ kvision-modules/kvision-tabulator/build.gradle.kts
@@ -13,7 +13,7 @@ kotlin {
 dependencies {
     api(rootProject)
     api(project(":kvision-modules:kvision-state"))
-    implementation(npm("tabulator-tables", "^5.2.3"))
+    implementation(npm("tabulator-tables", "^5.2.5"))
     testImplementation(kotlin("test-js"))
     testImplementation(project(":kvision-modules:kvision-testutils"))
 }
