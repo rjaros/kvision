36adb2a0c0d7d92cf748b09adcd27a37c9f13646
Robert Jaros <rjaros@finn.pl>
2022-11-25 09:23:03 +0100

    Suppres deprecation

--- kvision-modules/kvision-datacontainer/src/test/kotlin/test/io/kvision/data/DataContainerSpec.kt
+++ kvision-modules/kvision-datacontainer/src/test/kotlin/test/io/kvision/data/DataContainerSpec.kt
@@ -19,6 +19,8 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+@file:Suppress("DEPRECATION")
+
 package test.io.kvision.data
 
 import io.kvision.data.BaseDataComponent
