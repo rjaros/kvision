66f678cbfde4f0292e0f2c6a937e9598bee8f904
Robert Jaros <rjaros@finn.pl>
2023-01-28 14:51:49 +0100

    Version 6.1.0

--- kvision-tools/kvision-gradle-plugin/src/main/kotlin/io/kvision/gradle/KVisionPlugin.kt
+++ kvision-tools/kvision-gradle-plugin/src/main/kotlin/io/kvision/gradle/KVisionPlugin.kt
@@ -219,12 +219,12 @@ abstract class KVisionPlugin @Inject constructor(
 
         if (kvExtension.enableKsp.get()) {
             dependencies {
-                add("kspCommonMainMetadata", "io.kvision:kvision-ksp-processor:6.0.6")
+                add("kspCommonMainMetadata", "io.kvision:kvision-ksp-processor:6.1.0")
             }
 
             afterEvaluate {
                 dependencies {
-                    add("kspFrontend", "io.kvision:kvision-ksp-processor:6.0.6")
+                    add("kspFrontend", "io.kvision:kvision-ksp-processor:6.1.0")
                 }
                 kotlinMppExtension.sourceSets.getByName("commonMain").kotlin.srcDir("build/generated/ksp/metadata/commonMain/kotlin")
                 kotlinMppExtension.sourceSets.getByName("frontendMain").kotlin.srcDir("build/generated/ksp/frontend/frontendMain/kotlin")
