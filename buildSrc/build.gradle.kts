fd8e44f517b7c2bb0899e868084636c58c5809f5
Robert Jaros <rjaros@finn.pl>
2023-03-25 08:32:24 +0100

    Upgrade dokka plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,6 +10,6 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
     implementation("io.github.gradle-nexus:publish-plugin:1.2.0")
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.7.20")
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.8.10")
     implementation(gradleApi())
 }
