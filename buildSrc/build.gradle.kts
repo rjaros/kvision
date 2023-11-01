c7ae1d9942fdb4f90b7db49b372042ebece4b119
Robert Jaros <rjaros@finn.pl>
2023-11-01 07:47:57 +0100

    Upgrade dependencies (Gradle 8.4, Dokka 1.9.10, Jooby 3.0.6, Spring Boot 3.1.5, Ktor 2.3.5, Javalin 5.6.3, Vert.x 4.4.6, Micronaut 4.1.5, jackson-module-kotlin 2.15.3, kotlin-wrappers 636, bootstrap-icons 1.11.1, Tempus Dominus 6.7.16, Electron 27.0.2, Trix 2.0.7, TomSelect 2.3.0)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,6 +10,6 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
     implementation("io.github.gradle-nexus:publish-plugin:1.3.0")
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.0")
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.10")
     implementation(gradleApi())
 }
