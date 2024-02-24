93a7cb21f4052e0d6bac4b49ffacaebdcfb47783
Robert Jaros <rjaros@finn.pl>
2024-02-24 11:22:06 +0100

    Testing Kotlin-2.0.0-Beta4

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0-Beta4")
     implementation("io.github.gradle-nexus:publish-plugin:1.3.0")
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.10")
     implementation(gradleApi())
