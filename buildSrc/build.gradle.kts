c1ac2fa177da7e2c0194c323b5e6365d8fbd6fef
Robert Jaros <rjaros@finn.pl>
2024-03-17 13:51:06 +0100

    Upgraded dependencies (Kotlin 1.9.23, Dokka 1.9.20, KSP 1.0.19, Ktor 2.3.9, Javalin 6.1.3, Vert.x 4.5.5, Electron 4.3.6, jackson-module-kotlin 2.17.0, kotlin-wrappers 715, kotest 5.8.1, bootstrap-fileinput 5.5.3, electron 29.1.4)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,8 +8,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
     implementation("io.github.gradle-nexus:publish-plugin:1.3.0")
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.10")
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.20")
     implementation(gradleApi())
 }
